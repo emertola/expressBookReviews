e28e868c61a070c24a0aa010dbf8f5153e1d13d6
emertola <emertola@gmail.com>
2026-09-25 05:59:12 -0400

    Completed Task 11 using async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,24 +38,35 @@ public_users.get('/', async function (req, res) {
     };
     
     const bookList = await getBooks();
-    
+
     return res.status(200).send(JSON.stringify(bookList, null, 4));
   } catch (error) {
     return res.status(500).json({ message: "Error fetching book list" });
   }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// Get book details based on ISBN using Async-Await
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
 
-  if (books[isbn]) {
-    const bookByIsbn = JSON.stringify(books[isbn], null, 4);
-    return res.send(bookByIsbn);
-  }
+  try {
+    const getBook = () => {
+      return new Promise((resolve, reject) => {
+        if (books[isbn]) {
+          resolve(books[isbn]);
+        } else {
+          reject("Book not found");
+        }
+      });
+    };
 
-  return res.send('Book not found');
- });
+    const book = await getBook();
+    
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
